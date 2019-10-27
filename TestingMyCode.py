f2a856138641bd34b45a3aeb8ead326fa92b7e13
TemurKhabibullaev <55756791+TemurKhabibullaev@users.noreply.github.com>
2019-10-27 13:07:52 -0400

    Theories

--- TestingMyCode.py
+++ TestingMyCode.py
@@ -34,16 +34,13 @@ class NamesTestCase(unittest.TestCase):
 
 
 # Practicing Testing
-
+import unittest
 from names import city_country
 
 class testing_city_country(unittest.TestCase):
     def actual_testing(self):
         formatted_name_of_city = city_country('Oneonta', 'New York')
         self.assertEqual(formatted_name_of_city, 'Oneonta NY')
-
-
-unittest.main()
 
 # Assert methods:
 # assertEqual(a, b)          Verify that a == b
@@ -54,29 +51,7 @@ unittest.main()
 # assertNotIn(item, list)    Verify that item is not in list
 
 # Testing classes
-
-class AnonymousSurvey():
-    """Collect anonymous answers to a survey question."""
-
-    def __init__(self, question):
-        """Store a question, and prepare to store responses."""
-        self.question = question
-        self.responses = []
-
-    def show_question(self):
-        """Show the survey question."""
-        print(question)
-
-    def store_response(self, new_response):
-        """Store a single response to the survey."""
-        self.responses.append(new_response)
-
-    def show_results(self):
-        """Show all the responses that have been given."""
-        print("Survey results:")
-        for response in responses:
-            print('- ' + response)
-
+from names import AnonymousSurvey
 question = "What language did you first learn to speak?"
 my_survey = AnonymousSurvey(question)
 
@@ -92,3 +67,16 @@ while True:
 # Show the survey results.
 print("\nThank you to everyone who participated in the survey!")
 my_survey.show_results()
+
+class TestAnonymousSurvey(unittest.TestCase):
+    """Tests for the class AnonymousSurvey"""
+
+    def test_store_single_response(self):
+        """Test that a single response is stored properly."""
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        my_survey.store_response('English')
+
+        self.assertIn('English', my_survey.responses)
+
+unittest.main()
